effcceb1a7d325515e06b7b1b22155ceb293e58b
Tung Nguyen (BnK - HN) <tung.nguyen@bnksolution.com>
2021-03-26 11:24:22 +0700

    fix view file before upload

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -13,28 +13,13 @@ consumer.subscriptions.create("RoomChannel", {
         const current_id = parseInt(document.getElementById('user-id').value)
         const user_id = data.message.user_id
         if (user_id !== current_id) {
-          //   $(`<div class="message">
-          //   <div class="content_container">
-          //     <small class="text-muted">${data.username}</small>
-          //     <div class="content">
-          //        ${data.message.content}
-          //     </div>
-          //   </div>
-          // </div>`).appendTo('div.chat-room')
             $(data.html2).appendTo('div.chat-room')
-
         } else if (user_id === current_id) {
-          //   $(`<div class="message me">
-          //   <div class="content_container">
-          //     <div class="content">
-          //        ${data.message.content}
-          //     </div>
-          //   </div>
-          // </div>`).appendTo('div.chat-room')
             $(data.html).appendTo('div.chat-room')
 
             document.getElementById('message_content').value = ""
             document.getElementById('message_docs').value = ""
+            document.getElementById('preview').innerHTML = ""
         }
         $('#chat-room').scrollTop($('#chat-room')[0].scrollHeight);
     }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,9 +12,31 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-
 $(document).on('turbolinks:load', function () {
-    if ($('#chat-room').length>0){
+    if ($('#chat-room').length > 0) {
         $('#chat-room').scrollTop($('#chat-room')[0].scrollHeight);
     }
+
+    function previewImages() {
+        var $preview = $('#preview').empty();
+        if (this.files) $.each(this.files, readAndPreview);
+
+        function readAndPreview(i, file) {
+            if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+                // return alert(file.name + " is not an image");
+                var reader = new FileReader();
+                $(reader).on("load", function () {
+                    $preview.append($("<p/>", {text: file.name, height: 30}));
+                });
+                reader.readAsDataURL(file);
+            } else {
+                var reader = new FileReader();
+                $(reader).on("load", function () {
+                    $preview.append($("<img/>", {src: this.result, height: 100}));
+                });
+                reader.readAsDataURL(file);
+            }
+        }
+    }
+    $('#message_docs').on("change", previewImages);
 })
